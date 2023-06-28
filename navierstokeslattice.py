Fix streamline lengtht calculation and plot convergence of navier-stokes lattice

navierstokeslattice.py
@@ -12,11 +12,11 @@ from util import *
 
 """Simulation parameters"""
 # Create obstacle tensor from numpy array
-obstacle = generate_obstacle_tensor('input/pdrop/pdroplarge_3.png')
+obstacle = generate_obstacle_tensor('input/pdrop/pdrop3_20deg10.png')
 obstacle = obstacle.clone().to(device)
 nx, ny = obstacle.shape  # Number of nodes in x and y directions
-re = 20  # Reynolds number
-ulb = 0.004  # characteristic velocity (inlet)
+re = 10  # Reynolds number
+ulb = 0.001  # characteristic velocity (inlet)
 nulb = ulb * ny / re  # kinematic viscosity
 omega = 1 / (3 * nulb + 0.5)  # relaxation parameter
 print(f"omega: {omega}")
@@ -33,6 +33,8 @@ def equilibrium():
 # Initialize macroscopic variables
 rho = torch.ones((nx, ny), device=device).float()
 u = torch.zeros((2, nx, ny), device=device).float()
+last_u = torch.zeros((2, nx, ny), device=device).float()
+du = []
 
 # Initialize populations
 feq = torch.zeros((9, nx, ny), device=device).float()
@@ -90,13 +92,15 @@ def stream():
 
 
 def step():
-    global fin, fout, rho, u
+    global fin, fout, rho, u, last_u, du
     # Perform one LBM step
     # Outlet BC
     # Doing this first is more stable for some reason
     fin[left_col, -1, :] = fin[left_col, -2, :]
-
     macroscopic()  # Calculate macroscopic variables
+    # Calculate velocity difference
+    du.append(torch.norm(u - last_u).cpu().item())
+    last_u = u.clone()
     # Impose conditions on macroscopic variables
     u[0, 0, :] = ulb * torch.ones(ny, device=device).float()
     rho[0, :] = 1 / (1 - u[0, 0, :]) * (torch.sum(fin[center_col, 0, :], dim=0) +
@@ -154,12 +158,15 @@ def run(iterations: int, save_to_disk: bool = True, interval: int = 100, continu
                 counter = 0
 
             counter += 1
-            bar.text(f"MLUPS: {mlups:.2f}")
+            bar.text(f"MLUPS: {mlups:.2f}, du: {du[-1]:.5e}")
             bar()
 
     # Save final data to numpy files
     np.save(f"output/BaseLattice_last_u.npy", u.cpu().numpy())
     np.save(f"output/BaseLattice_last_rho.npy", rho.cpu().numpy())
+    fig, ax = plt.subplots()
+    ax.semilogy(np.asarray(du[2:]))
+    plt.show()
 
     if save_to_disk:
         # Stop thread for saving data
@@ -169,4 +176,4 @@ def run(iterations: int, save_to_disk: bool = True, interval: int = 100, continu
 
 if __name__ == '__main__':
     print("Using device: ", device)
-    run(25000, save_to_disk=True, interval=1000, continue_last=False)
+    run(10000, save_to_disk=True, interval=1000, continue_last=False)

poregen_gauss.py
@@ -9,8 +9,8 @@ from PIL import Image
 
 cmap = matplotlib.colormaps['Greys']
 
-nx, ny = 404, 4042  # domain dimensions
-target_rho = 0.5
+nx, ny = 300, 300  # domain dimensions
+target_rho = 0.7
 
 
 # Custom gaussian kernel with multivariate normal distribution and custom covariance matrix
@@ -47,7 +47,7 @@ cov = np.asarray([[1, 0],
                   [0, 1]])  # Covariance matrix
 
 # Generate the kernel
-kernel = gaussian_kernel(size, mean, .1 * cov, angle=np.pi / 9)
+kernel = gaussian_kernel(size, mean, .01 * cov, angle=np.pi / 9)
 
 plt.imshow(kernel, interpolation='none')
 plt.axis('off')
@@ -65,6 +65,7 @@ cutoff = norm.ppf(target_rho, loc=np.mean(smooth), scale=np.std(smooth))
 
 # Generate boolean matrix
 final = smooth > cutoff
+print(f'Porosity: {1 - np.sum(final) / (nx * ny)}')
 
 # plt.imshow(final.T, cmap=cmap, interpolation='none')
 # plt.axis('off')
@@ -74,4 +75,4 @@ final = smooth > cutoff
 rgb_array = 255 * np.ones((nx, ny, 3))
 rgb_array[final, :] = np.asarray([0, 0, 0])
 image = Image.fromarray(rgb_array.transpose(1, 0, 2).astype(np.uint8), mode='RGB')
-image.save(f'output/electrode2.png')
+image.save(f'output/pdrop_001sig.png')

streamlines.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from util import *
 
-v = np.load("input/mmrfb_u.npy")
-obstacle = generate_obstacle_tensor("input/mmrfb_original.png").cpu().numpy()
+v = np.load("output/BaseLattice_last_u.npy")
+obstacle = generate_obstacle_tensor("input/pdrop/pdrop3_60deg1.png").cpu().numpy()
 v[:, obstacle] = 0
 
 d, nx, ny = v.shape
-x = np.linspace(0, 1000, nx)
-y = np.linspace(0, 872, ny)
+x = np.arange(nx)
+y = np.arange(ny)
 X, Y = np.meshgrid(x, y)
 #
 U = np.transpose(v[0, :, :])
@@ -16,7 +16,8 @@ V = np.transpose(v[1, :, :])
 M = np.hypot(U, V)
 magnitude = np.sqrt(U ** 2 + V ** 2)
 n = 5
-plt.imshow(obstacle.T == True, cmap='Greys')
-plt.streamplot(X, Y, U, V, density=20, linewidth=.1, arrowstyle='-', color=magnitude, cmap=cmap, broken_streamlines=False)
+plt.imshow(obstacle.T, cmap='Greys')
+plt.streamplot(X, Y, U, V, density=2, linewidth=.1, arrowstyle='-', color=magnitude, cmap=cmap, broken_streamlines=False)
 
 plt.savefig('output/test_3.png', dpi=1500)
+# plt.show()

tortuosity_measurement.py
@@ -1,20 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.linalg import norm
-from numpy import gradient, sqrt
-from scipy import integrate
 from scipy.interpolate import RegularGridInterpolator
 from util import *
 
 v = np.load("output/BaseLattice_last_u.npy")
-obstacle = generate_obstacle_tensor("input/pdrop/pdroplarge_3.png").cpu().numpy()
+obstacle = generate_obstacle_tensor("input/pdrop/pdrop3_20deg10.png").cpu().numpy()
+left_margin = 10
 v[:, obstacle] = 0
 U = v[0]
 V = v[1]
 x = np.arange(v.shape[1])
 y = np.arange(v.shape[2])
 
-# Interpolation of vector field
+# Interpolation of vector field 244 74
 U_interp = RegularGridInterpolator((x, y), U)
 V_interp = RegularGridInterpolator((x, y), V)
 
@@ -38,14 +37,21 @@ for i in range(0, v.shape[2], 5):
     while True:
         t = -100  # step backwards
         # Calculate next step from previous step using euler method
-        next_step = path[-1] + np.asarray(func(path[-1])) * t
+        try:
+            next_step = path[-1] + np.asarray(func(path[-1])) * t
+        except ValueError:
+            print("ValueError")
+            break
         path.append(next_step)
-        if path[-1][0] < 20:  # TODO: Consider this when calculating reference length
+        if path[-1][0] < left_margin:  # if reached left boundary
             paths.append(path)
+            # print("reached end")
             break
-        if np.abs(path[-1][1] - 0) < 10 or np.abs(path[-1][1] - v.shape[2]) < 10:  # if reached top boundary
+        if np.abs(path[-1][1] - 0) < 1 or np.abs(path[-1][1] - v.shape[2]) < 1:  # if reached top boundary
+            print("reached top/bot boundary")
             break
-        if norm(path[-1] - path[-2]) < 1e-6 or len(path) > 100000:  # If reached end of streamline (v = 0)
+        if norm(path[-1] - path[-2]) < 1e-16 or len(path) > 100000:  # If reached end of streamline (v = 0)
+            print("reached velocity ~zero")
             break
         print(f"Progress: {int(i * 100 / v.shape[2])}%, \tCurrent x: {path[-1][0]}\tCurrent y: {path[-1][1]}", sep='', end="\r", flush=True)
 
@@ -64,9 +70,9 @@ for path in paths:
     ax.plot(np.asarray(path)[:, 0], np.asarray(path)[:, 1], 'r--', linewidth=0.2)
 
 plt.imshow(np.invert(obstacle.T), cmap='gray')
-plt.savefig("output/Streamlines.png", dpi=1200)
+plt.savefig("output/Streamlines_pdrop3_20deg10.png", dpi=1200)
 
 lengths = np.asarray(lengths)
-tortuosity = np.mean(lengths) / v.shape[1]
+tortuosity = np.mean(lengths) / (v.shape[1] - left_margin - 1)  # -1 for right margin
 
 print(f"Measured Tortuosity: {tortuosity}")